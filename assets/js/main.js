1a1fef1f0d929758974ead1830d22509006c9bb1
Mahyar Kaliji <mahyar.kaliji@gmail.com>
2024-08-18 00:04:49 +0330

    test2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// popular posts section swiper
 const popularSwiper = new Swiper(".swiper.popular-posts", {
   // Optional parameters
   loop: true,
@@ -33,6 +34,7 @@ const popularSwiper = new Swiper(".swiper.popular-posts", {
   },
 });
 
+// new posts section swiper
 const newPostSwiper = new Swiper(".swiper.new-posts", {
  
   loop: true,
@@ -55,6 +57,7 @@ const newPostSwiper = new Swiper(".swiper.new-posts", {
   },
 })
 
+// trendy posts section swiper
 const trendySwiper = new Swiper(".swiper.trendy-posts", {
   // Optional parameters
   loop: true,
@@ -90,3 +93,80 @@ const trendySwiper = new Swiper(".swiper.trendy-posts", {
     },
   },
 });
+
+// popular posts section swiper
+const topSwiper = new Swiper(".swiper.top-posts", {
+  // Optional parameters
+  loop: true,
+
+    // autoplay: {
+    //   delay: 3000,
+    // },
+
+  breakpoints: {
+    350: {
+      spaceBetween: 20,
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 1.8,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 2.5,
+    },
+    1280: {
+      slidesPerView: 3,
+      // centeredSlides: true,
+    },
+    1520: {
+      slidesPerView: 3.5,
+    },
+    1920: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+  },
+});
+
+
+const topTagsSwiper = new Swiper(".swiper.top-tags", {
+  // Optional parameters
+  loop: true,
+
+    // autoplay: {
+    //   delay: 3000,
+    // },
+
+    spaceBetween: 24,
+    slidesPerView: 7,
+  // breakpoints: {
+  //   350: {
+  //     spaceBetween: 20,
+  //     slidesPerView: 1,
+  //   },
+  //   640: {
+  //     slidesPerView: 1.8,
+  //   },
+  //   768: {
+  //     slidesPerView: 2,
+  //   },
+  //   1024: {
+  //     slidesPerView: 2.5,
+  //   },
+  //   1280: {
+  //     slidesPerView: 3,
+  //     // centeredSlides: true,
+  //   },
+  //   1520: {
+  //     slidesPerView: 3.5,
+  //   },
+  //   1920: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 24,
+  //   },
+  // },
+});
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/*.{html,js}"],
+  content: ["./pages/*.{html,js}", "./*.{html,js}"],
   theme: {
     extend: {
       backgroundPosition: {
@@ -12,14 +12,31 @@ module.exports = {
         dark: "#3E3232",
         primary: "rgba(245, 21, 57, .75)",
         dark5: "rgba(62, 50, 50, .05)",
+        dark50: "rgba(62, 50, 50, .5)",
+        dark75: "rgba(62, 50, 50, .75)",
         play_btn_bg: "rgba(255, 255, 255, .75)",
         redGrad: "linear-gradient(to left, #F81539, #FC4308)",
+        insta_active: "rgba(248, 21, 57, 0.75)",
+        hashtag: "rgba(0, 0, 0, 0.2)",
       },
       backgroundImage: {
         sportWidget: "url('../images/sport/06.jpg')",
-        weatherImg: "url('../images/weather/04.jpg')",
-        weatherGrdt:
+        weatherImg1: "url('../images/weather/04.jpg')",
+        weatherImg2: "url('../images/city/03.jpg')",
+        weatherImg3: "url('../images/weather/12.jpg')",
+        weatherImg4: "url('../images/city/01.jpg')",
+        weatherGrdt1:
           "linear-gradient(to bottom right, rgba(251, 68, 28, .91), rgba(251, 202, 28, .6))",
+        weatherGrdt2:
+          "linear-gradient(to bottom right, rgba(28, 50, 251, 0.91), rgba(28, 251, 224, .6))",
+        weatherGrdt3:
+          "linear-gradient(to bottom right, rgba(7, 156, 39, .91), rgba(192, 255, 113, .6))",
+        weatherGrdt4:
+          "linear-gradient(to bottom right, rgba(113, 28, 251, .91), rgba(251, 28, 148, .6))",
+        insta_hover: "linear-gradient(to bottom left, #F45C9F , #FF7563 )",
+        twitter: "linear-gradient(to bottom left, #2CA5E0 , #67C9F5 )",
+        topTagsGradient:
+          "radial-gradient(circle at 70% 50%, rgba(245, 245, 245, 1) 52%, rgba(245, 245, 245, 0.3827) 82%, rgba(245, 245, 245, 0) 100%)",
       },
     },
     screens: {
